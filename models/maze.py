e64995b445eb44f7557ce888d895879a79eb6045
itsanhkhoi12 <anhkhoi5602@gmail.com>
2026-06-21 12:02:01 +0700

    feat: first added Maze and Robot models

---
+++ models/maze.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel, Field
+from dataclasses import dataclass
+
+# Định nghĩa vị trí theo ô (x;y)
+
+@dataclass(frozen=True)
+class Point:
+    x: int
+    y: int
+
+class Maze(BaseModel):
+    id: str = ''
+    rows: int 
+    cols: int 
+    
+    # Grid tức là field định nghĩa map của mê cung
+        # 0 là đường đi
+        # 1 là tường
+    grid: list[list[int]] = Field(default_factory = list)
+
+    # Điểm bắt đầu của mê cung
+    start_pos: Point
+
+    # Điểm kết thúc của mê cung
+    end_pos: Point
+
